f3eb63d9db039f9088243b7eee6ef8f25e07953b
Samir Badi <samirbadi1@outlook.com>
2021-12-15 17:14:46 -0500

    Create the variable for the class Civilisation.

--- src/main/java/org/example/Info/Civilisation.java
+++ src/main/java/org/example/Info/Civilisation.java
@@ -1,5 +1,11 @@
 package org.example.Info;
 
 public class Civilisation {
+    private String name;
+    private String speciality;
+    private String bonus;
+    private String uniqueUnity;
+    private String uniqueTechnology;
+
 
 }
