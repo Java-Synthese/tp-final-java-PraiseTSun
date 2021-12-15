67d1c7567f595e97692958fce679a808eb09ad42
Samir Badi <samirbadi1@outlook.com>
2021-12-15 16:57:13 -0500

    Create the variable for the class Unit.

--- src/main/java/org/example/Info/Unit.java
+++ src/main/java/org/example/Info/Unit.java
@@ -1,4 +1,31 @@
 package org.example.Info;
 
+import java.util.List;
+
 public class Unit {
+    private enum AGES {
+        UNKNOW(0),
+        Dark_Age(1),
+        Feudal_Age(2),
+        Castle_Age(3),
+        Imperial_Age(4);
+
+        private final int value;
+
+        AGES(final int newValue){
+            value = newValue;
+        }
+
+        public int getvalue(){return value;}
+    }
+
+    private String name;
+    private List<AGES> allAges;
+    private String unitBatiment;
+    private List<String> unitCost;
+    private String buildingTime;
+    private String livingPoint;
+
+
+
 }
