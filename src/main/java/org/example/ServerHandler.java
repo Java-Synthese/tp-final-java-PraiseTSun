864585e89562e5825910f61f72d70811b1304685
Samir Badi <samirbadi1@outlook.com>
2021-12-15 16:03:30 -0500

    Add the mothod argumentHandler at ServerHandler.

--- src/main/java/org/example/ServerHandler.java
+++ src/main/java/org/example/ServerHandler.java
@@ -1,5 +1,21 @@
 package org.example;
 
-public class ServerHandler {
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class ServerHandler {
+    private int serverPort;
+    private Path dataFolder;
+
+    public void argumentsHandler(String[] arguments){
+        if(arguments.length < 7)
+            throw new RuntimeException("There are some missing arguments");
+
+        serverPort = Integer.parseInt(arguments[2]);
+
+        if(!Files.isDirectory(Paths.get(arguments[4])))
+            throw new RuntimeException(arguments[4] + " is not the data folder path.");
+        dataFolder = Paths.get(arguments[4]);
+    }
 }
