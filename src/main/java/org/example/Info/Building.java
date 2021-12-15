f040fb4df519a7a9eab5b9aa81a5dcfa2d003f34
Samir Badi <samirbadi1@outlook.com>
2021-12-15 17:26:52 -0500

    Create the variable for the class Building.

--- src/main/java/org/example/Info/Building.java
+++ src/main/java/org/example/Info/Building.java
@@ -1,4 +1,34 @@
 package org.example.Info;
 
+import java.util.ArrayList;
+
 public class Building {
+    private enum AGES {
+        UNKNOW(0),
+        Dark_Age(1),
+        Feudal_Age(2),
+        Castle_Age(3),
+        Imperial_Age(4);
+
+        private final int value;
+
+        AGES(final int newValue){
+            value = newValue;
+        }
+
+        public int getvalue(){return value;}
+    }
+
+    private String name;
+    private ArrayList<Integer> ages;
+    private String type;
+    private ArrayList<String> cost;
+    private String time;
+    private int hitPoint;
+    private int visibility;
+    private ArrayList<String> civilisations;
+
+
+
+
 }
