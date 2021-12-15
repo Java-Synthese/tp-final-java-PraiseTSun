ba27a759caf2feeee5f88b1b502ac846f6be43c7
Samir Badi <samirbadi1@outlook.com>
2021-12-15 17:15:23 -0500

    Added the Civilisation's contructor.

--- src/main/java/org/example/Info/Civilisation.java
+++ src/main/java/org/example/Info/Civilisation.java
@@ -7,5 +7,13 @@ public class Civilisation {
     private String uniqueUnity;
     private String uniqueTechnology;
 
+    public Civilisation(String name, String speciality, String bonus, String uniqueUnity, String uniqueTechnology) {
+        this.name = name;
+        this.speciality = speciality;
+        this.bonus = bonus;
+        this.uniqueUnity = uniqueUnity;
+        this.uniqueTechnology = uniqueTechnology;
+    }
+
 
 }
