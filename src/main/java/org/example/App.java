2f59a442bed662f0f49144a6819a45172c9658fe
Samir Badi <samirbadi1@outlook.com>
2021-12-21 22:20:03 -0500

    Fixed args

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,12 +5,12 @@ public class App {
         //String[] arg = {"--server", "-port", "8888", "--data", "data/", "--threads", "6"};
         if(args[0].equals("--server")) {
             ServerHandler server = new ServerHandler();
-            server.setUpServer(arg);
+            server.setUpServer(args);
         }
         //arg = new String[] {"java", "-jar", "votreApp.jar", "localhost:8888", "get-units", "Archer"};
         if (args[0].equals("java")){
             ClientHandler client = new ClientHandler();
-            client.setUpServer(arg);
+            client.setUpServer(args);
         }
     }
 }
